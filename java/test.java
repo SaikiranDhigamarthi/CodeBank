2bffb2ecb4df042f1222a2fbc70569403a39513f
Sai Kiran Dhigamarthi <dsaikiran@OJAS-IT.COM>
2018-06-16 11:13:26 +0530

    FIRST GIT

---
+++ java/test.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ShipmentCompanyDetails {
+
+	public static void main(String[] args) {
+
+		Map<Integer, String> map;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter The Number Of Cargos:");
+		int noOfCargos = sc.nextInt();
+
+		System.out.println("Enter The Cargo ID:");
+		Integer integerArray[] = new Integer[noOfCargos];
+		for (int i = 0; i < noOfCargos; i++) {
+			integerArray[i] = sc.nextInt();
+		}
+
+		System.out.println("Enter The Number Of Shipments:");
+		int noOfShipments = sc.nextInt();
+
+		System.out.println("Enter The Shipment name");
+		String strArray[] = new String[noOfShipments];
+		for (int i = 0; i < noOfShipments; i++) {
+			strArray[i] = sc.next();
+
+		}
+		map = new HashMap<Integer, String>();
+		if (noOfCargos > noOfShipments) {
+			for (int i = 0; i < noOfShipments; i++) {
+				map.put(integerArray[i], strArray[i]);
+			}
+		} else {
+			for (int i = 0; i < noOfCargos; i++) {
+				map.put(integerArray[i], strArray[i]);
+			}
+		}
+
+		System.out.println("Enter The Cargo ID Number");
+		int cargoId = sc.nextInt();
+		String id = map.get(cargoId);
+		if (id == null) {
+			System.out.println("Yet To Be Shipped!!!!");
+		} else {
+			System.out.println(id);
+		}
+
+	}
+
+}
